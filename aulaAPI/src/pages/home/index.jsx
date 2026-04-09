c965856e1d510e2b09148f61cee61f7affdffdaf
Danilo <danilodelimasantanacampos@gmail.com>
2026-04-09 20:17:46 +0000

    aula01-base do projeto

---
+++ aulaAPI/src/pages/home/index.jsx
@@ -0,0 +1,10 @@
+function Home() {
+    return (
+        <div>
+            <h1>Seja bem-viado</h1>
+            <p>Vamos usar a PokeAPI</p>
+        </div>
+    )
+}
+
+export default Home
---
+++ aulaAPI/src/pages/pokemon/index.jsx
@@ -0,0 +1,10 @@
+function Pokemon() {
+
+    return (
+        <div>
+            <h1>Lista de Pokémon</h1>
+        </div>
+    )
+}
+
+export default Pokemon
