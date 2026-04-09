60ec4970bcce3d88c709838d9362e43cacba1f72
Danilo <danilodelimasantanacampos@gmail.com>
2026-04-09 20:30:03 +0000

    aula01-Adicionando novos pokemon

--- aulaAPI/src/pages/pokemon/index.jsx
+++ aulaAPI/src/pages/pokemon/index.jsx
@@ -1,3 +1,16 @@
+import { useState } from "react"
+
+const pokemonList = [
+    {id:1, nome:"Bulbassauro"},
+    {id:4, nome:"Charmander"},
+    {id:428, nome:"Lopunny"},
+    {id:132, nome:"Ditto"},
+    {id:654, nome:"Braixen"},
+    {id:700, nome:"Sylveon"},
+    {id:282, nome:"Gardevoir"},
+    {id:815, nome:"Cinderace"}
+]
+
 function Pokemon() {
 
     return (
